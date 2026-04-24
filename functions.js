58faf39e9fd389bb13e2ff8e3e9b37d4fcaeaa07
GP-codes <pokharelgaurav1012@gmail.com>
2026-04-24 07:03:37 -0400

    Added descriptive pages
    Added separate pages for each 'View Details' button under the artifafcts.

---
+++ functions.js
@@ -0,0 +1,24 @@
+function details(itemName)
+
+{
+window.location.href = itemName+ ".html";
+}
+
+
+function cart(itemName)
+
+{
+
+}
+
+function buy(itemName)
+
+{
+
+}
+
+function negotiate(itemName)
+
+{
+
+}
