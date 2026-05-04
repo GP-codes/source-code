b627016f002df4a3af7af02b0198aac27ce526e3
GP-codes <pokharelgaurav1012@gmail.com>
2026-05-04 14:14:54 -0400

    Implemented checkout function
    added functionality to checkout.html page, improved checkout function and added new function to remove items from cart

--- functions.js
+++ functions.js
@@ -17,6 +17,10 @@ var price_list = [25,30,100,20,450,500,400,600,150,125,175,200]
 function cart(itemName)
 {
 
+var currentPrice = 0;
+var currentName = "";
+var currentImage = "";
+
 
 if (localStorage.getItem("totalItems") !== null) 
 {
@@ -35,53 +39,108 @@ switch(itemName)
 {
 case "love":
 price += price_list[0];
+currentPrice = price_list[0];
+currentName = "Love Spell";
+currentImage = "media/Love Spell.png";
 break;
 
 case "rage":
 price += price_list[1];
+currentPrice = price_list[1];
+currentName = "Rage Spell";
+currentImage = "media/Rage Spell.png";
 break;
 
 case "wealth":
 price += price_list[2];
+currentPrice = price_list[2];
+currentName = "Wealth Spell";
+currentImage = "media/Wealth Spell.png";
 break;
 
 case "intelligence":
 price += price_list[3];
+currentPrice = price_list[3];
+currentName = "Intelligence Spell";
+currentImage = "media/Intelligence Spell.png";
 break;
 
 case "axe":
 price += price_list[4];
+currentPrice = price_list[4];
+currentName = "Enchanted Axe";
+currentImage = "media/Enchanted Axe.png";
 break;
 
 case "bow":
 price += price_list[5];
+currentPrice = price_list[5];
+currentName = "Enchanted Bow";
+currentImage = "media/Enchanted Bow.png";
 break;
 
 case "club":
 price += price_list[6];
+currentPrice = price_list[6];
+currentName = "Enchanted Club";
+currentImage = "media/Enchanted Club.png";
 break;
 
 case "sword":
 price += price_list[7];
+currentPrice = price_list[7];
+currentName = "Enchanted Sword";
+currentImage = "media/Enchanted Sword.png";
 break;
 
 case "dragon":
 price += price_list[8];
+currentPrice = price_list[8];
+currentName = "Hatchling Dragon";
+currentImage = "media/Hatchling Dragon.png";
 break;
 
 case "chick":
 price += price_list[9];
+currentPrice = price_list[9];
+currentName = "Phoenix Chick";
+currentImage = "media/Phoenix Chick.png";
 break;
 
 case "cat":
 price += price_list[10];
+currentPrice = price_list[10];
+currentName = "Spectral Cat";
+currentImage = "media/Spectral Cat.png";
 break;
 
 case "unicorn":
 price += price_list[11];
+currentPrice = price_list[11];
+currentName = "Unicorn Foal";
+currentImage = "media/Unicorn Foal.png";
 break;
 
 }
+
+var purchasedItem = 
+{
+id: itemName,
+name: currentName,
+price: currentPrice,
+image: currentImage,
+quantity:1
+};
+
+var detailedList = [];
+
+if(localStorage.getItem("detailedCart") !== null)
+{
+detailedList = JSON.parse(localStorage.getItem("detailedCart"));
+}
+
+detailedList.push(purchasedItem);
+localStorage.setItem("detailedCart", JSON.stringify(detailedList));
 
 
 localStorage.setItem("totalItems", numItems)
@@ -102,7 +161,7 @@ cartText.innerHTML = numItems;
 
 function checkout()
 {
-var savedItems = localStorage.getItem("totalItems");
+/* var savedItems = localStorage.getItem("totalItems");
 var savedPrice = localStorage.getItem("totalPrice");
 
 if (savedItems === null)
@@ -112,9 +171,91 @@ savedPrice = 0;
 }
 
 document.getElementById('titems').innerHTML = savedItems;
-document.getElementById('tprice').innerHTML = savedPrice;
+document.getElementById('tprice').innerHTML = savedPrice; 
+
+
+*/
+
+var savedCart = localStorage.getItem("detailedCart");
+var container = document.getElementById("cart-container");
+var summary = document.getElementById("cart-summary");
+
+if(savedCart === null || savedCart ==="[]")
+{
+container.innerHTML = "<h2 id='header-text'>Your cart is currently empty!</h2>";
+summary.style.display = "none";
+return;
 }
 
+var cartArray = JSON.parse(savedCart);
+
+var subTotal = 0;
+var shipping = 10;
+
+container.innerHTML ="";
+
+for(var i = 0; i < cartArray.length; i++)
+{
+var item = cartArray[i];
+var itemPrice = item.price * item.quantity;
+subTotal += itemPrice;
+
+
+container.innerHTML += `
+<div style="border: 1px solid black; padding: 10px; margin-bottom: 10px; overflow:hidden ;">
+
+<img src= "${item.image}" style="width:50px; height:50px; float:left; margin-right: 15px;">
+<h4 id='body-text'>${item.name}</h4>
+<p>Price: $${item.price} | Quantity: ${item.quantity}</p>
+
+<input type="button" value="Remove Item" onclick="removeItem(${i})" class="body-text">
+
+`;
+
+}
+
+var taxAmount = .06*subTotal;
+var finalTotal = taxAmount + subTotal + shipping;
+
+document.getElementById("subtotal").innerHTML = subTotal.toFixed(2);
+document.getElementById("tax").innerHTML = taxAmount.toFixed(2);
+document.getElementById("gtotal").innerHTML = finalTotal.toFixed(2);
+
+
+}
+
+
+function removeItem(index)
+{
+var cartArray = JSON.parse(localStorage.getItem("detailedCart"));
+
+cartArray.splice(index, 1);
+
+localStorage.setItem("detailedCart", JSON.stringify(cartArray));
+
+
+var newNumItems = 0;
+var newPrice = 0;
+
+for (var i=0; i < cartArray.length; i++)
+{
+newNumItems += cartArray[i].quantity;
+newPrice += cartArray[i].price;
+}
+
+localStorage.setItem("totalItems", newNumItems);
+localStorage.setItem("totalPrice", newPrice);
+
+var cartText = document.getElementById("cart");
+if(cartText !== null)
+{
+cartText.innerHTML = newNumItems;
+}
+
+
+checkout();
+
+}
 
 
 
